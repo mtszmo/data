305c6ffa5262236c0e644cef7018f8d8e70d6ad8
mtszmo <matiaszamo52@gmail.com>
2026-07-08 15:43:50 -0300

    Gráficos

--- queries/04_estados_mas_facturan.sql
+++ queries/04_estados_mas_facturan.sql
@@ -5,5 +5,4 @@ FROM customers c
 JOIN orders o ON c.customer_id = o.customer_id
 JOIN order_payments op ON o.order_id = op.order_id
 GROUP BY c.customer_state
-ORDER BY total_facturado DESC
-LIMIT 5;
+ORDER BY total_facturado DESC;
